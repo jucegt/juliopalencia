3eba74a498f8bdc4c21ac797288dc1276299aa8b
jucegt <julio2201@gmail.com>
2025-04-16 23:57:12 -0600

    feat: create link component for header with styling and utility function

---
+++ src/components/header/link.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { cn } from '../../utilities/cn';
+</script>
+
+<a
+  {...$$restProps}
+  class={cn(
+    'text-gray hover:text-primary focus-visible:ring-primary rounded-sm px-2 transition focus:outline-none focus-visible:ring-2',
+    $$restProps.class
+  )}
+>
+  <slot />
+</a>
