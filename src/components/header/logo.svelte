f7fa80ef91c7807984bbdac6ac81bb1bfdd74490
jucegt <julio2201@gmail.com>
2025-04-16 23:54:44 -0600

    feat: add logo component to header

---
+++ src/components/header/logo.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import Logo from '../../svgs/logo.svelte';
+</script>
+
+<a
+  href="/"
+  class="focus-visible:ring-primary flex items-center rounded-sm p-1 focus:outline-none focus-visible:ring-2"
+  aria-label="Julio Palencia"
+>
+  <Logo class="text-primary block h-[32px] w-[48px]" />
+</a>
