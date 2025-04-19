d3b261d8f8eece59f73262da565463ec78b1d533
jucegt <julio2201@gmail.com>
2025-04-19 00:19:40 -0600

    feat: add shiny-text component for gradient text styling

---
+++ src/components/shiny-text.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { cn } from '../utilities/cn';
+
+  export let as: keyof HTMLElementTagNameMap = 'span';
+</script>
+
+<svelte:element
+  this={as}
+  {...$$restProps}
+  class={cn(
+    'from-gradient-start to-gradient-end bg-gradient-to-r bg-clip-text font-bold text-transparent',
+    $$restProps.class
+  )}
+>
+  <slot />
+</svelte:element>
