ac641e54f233130f5cf9ae7c74f45dbc2be384cf
jucegt <julio2201@gmail.com>
2025-04-18 20:20:55 -0600

    feat: add z-index to logo link for improved stacking context

--- src/components/header/logo.svelte
+++ src/components/header/logo.svelte
@@ -4,7 +4,7 @@
 
 <a
   href="/"
-  class="focus-visible:ring-primary -mx-2 flex items-center rounded-sm p-2 focus:outline-none focus-visible:ring-2"
+  class="focus-visible:ring-primary z-10 -mx-2 flex items-center rounded-sm p-2 focus:outline-none focus-visible:ring-2"
   aria-label="Julio Palencia"
 >
   <Logo class="text-primary block h-[32px] w-[48px]" />
