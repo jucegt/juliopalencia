aa6831a347fbc8ce08610bb5e4d3d7370b7bf401
jucegt <julio2201@gmail.com>
2025-04-18 20:20:11 -0600

    feat: update container component with responsive padding for improved layout

--- src/components/container.svelte
+++ src/components/container.svelte
@@ -2,6 +2,6 @@
   import { cn } from '../utilities/cn';
 </script>
 
-<div {...$$restProps} class={cn('container mx-auto', $$restProps.class)}>
+<div {...$$restProps} class={cn('container mx-auto px-4 lg:px-8', $$restProps.class)}>
   <slot />
 </div>
