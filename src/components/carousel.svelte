399836e9736ab658da1ff2ea453f6663c2095ef4
jucegt <julio2201@gmail.com>
2025-04-19 17:47:04 -0600

    feat: add carousel component using embla-carousel-svelte for enhanced user experience

---
+++ src/components/carousel.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { type EmblaOptionsType } from 'embla-carousel';
+  import emblaCarouselSvelte from 'embla-carousel-svelte';
+
+  let params = {
+    options: {
+      align: 'center',
+      breakpoints: {
+        '(min-width: 1280px)': {
+          active: false
+        }
+      }
+    } as EmblaOptionsType,
+    plugins: []
+  };
+</script>
+
+<div use:emblaCarouselSvelte={params}>
+  <div class="flex gap-x-8">
+    <slot />
+  </div>
+</div>
