27007a553a6e1c42ff5a369d60c9c9938c11d39f
jucegt <julio2201@gmail.com>
2025-04-18 23:38:27 -0600

    feat: reorganize component imports and rendering order in the main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,17 @@
 <script>
+  import Benefits from '../components/benefits.svelte';
+  import Contact from '../components/contact.svelte';
+  import Experience from '../components/experience.svelte';
   import Header from '../components/header.svelte';
   import Hero from '../components/hero.svelte';
+  import Services from '../components/services.svelte';
+  import Testimonials from '../components/testimonials.svelte';
 </script>
 
 <Header />
 <Hero />
+<Benefits />
+<Services />
+<Testimonials />
+<Experience />
+<Contact />
