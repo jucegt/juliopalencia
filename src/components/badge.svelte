3a8d7f7fc69052f9b769134f7d9370fb98fc2d47
jucegt <julio2201@gmail.com>
2025-04-18 23:25:50 -0600

    feat: add badge component for reusable UI element

---
+++ src/components/badge.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { cn } from '../utilities/cn';
+
+  export let as: keyof HTMLElementTagNameMap = 'h1';
+</script>
+
+<svelte:element
+  this={as}
+  {...$$restProps}
+  class={cn(
+    'border-primary text-primary bg-primary/8 font-onest w-fit rounded-full border px-2 py-1 text-(length:--font-xxs) leading-none font-bold uppercase',
+    $$restProps.class
+  )}
+>
+  <slot />
+</svelte:element>
