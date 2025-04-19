877cfee17bf08df13b3fc656a18318cfccc70f2f
jucegt <julio2201@gmail.com>
2025-04-18 23:26:24 -0600

    feat: add header component for structured section layout

---
+++ src/components/section/header.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import Badge from '../badge.svelte';
+  import Title from '../title.svelte';
+
+  export let badge: string | undefined = undefined;
+  export let title: string | undefined = undefined;
+</script>
+
+{#if badge}
+  <Badge class="mb-4 md:mx-auto">{badge}</Badge>
+{/if}
+{#if title}
+  <Title>{title}</Title>
+{/if}
+<p class="mb-8 text-base md:mb-16 md:text-center md:text-xl md:text-balance"><slot /></p>
