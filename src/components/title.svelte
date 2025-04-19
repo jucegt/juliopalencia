f75c253ea82d210e50a26538c7cbf61494094c9c
jucegt <julio2201@gmail.com>
2025-04-18 23:26:04 -0600

    feat: create title component for flexible heading usage

---
+++ src/components/title.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let as: keyof HTMLElementTagNameMap = 'h2';
+</script>
+
+<svelte:element
+  this={as}
+  class="text-dark dark:text-light font-onest mb-8 text-2xl font-black sm:text-3xl md:text-center md:text-5xl"
+>
+  <slot />
+</svelte:element>
