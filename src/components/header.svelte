f71ba7cd6cf85e0ff04c164461d379f2946e12ee
jucegt <julio2201@gmail.com>
2025-04-18 20:20:43 -0600

    feat: enhance header component with improved backdrop styling and accessibility

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -8,13 +8,15 @@
   import Right from './header/right.svelte';
 </script>
 
-<header class="bg-light/80 dark:bg-dark/80 sticky top-0 z-50 backdrop-blur-sm">
-  <Container class="flex w-full items-center justify-between px-4 py-4 md:py-8">
+<header
+  class="before:content-'' before:bg-light/70 before:dark:bg-dark/70 sticky top-0 z-50 before:absolute before:inset-0 before:opacity-0 before:backdrop-blur-sm"
+>
+  <Container class="flex w-full items-center justify-between py-4 md:py-8">
     <Logo />
     <Nav />
     <Right>
       <Button variant="secondary" href="#contact">
-        <Mail width="24" class="z-10 shrink-0 grow-0 basis-[24px]" />
+        <Mail width="24" class="z-10 shrink-0 grow-0 basis-[24px]" aria-hidden="true" />
         <span class="z-10">
           Let’s talk <span class="hidden md:inline">about your website</span>
         </span>
