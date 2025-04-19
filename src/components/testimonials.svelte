a725598fa18e991533b9c030d3138f16b5835904
jucegt <julio2201@gmail.com>
2025-04-18 23:31:21 -0600

    feat: add testimonials component to display client feedback

---
+++ src/components/testimonials.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Container from './container.svelte';
+  import Header from './section/header.svelte';
+</script>
+
+<Container as="section" class="my-24 flex flex-col md:my-48">
+  <Header badge="Testimonials" title="Client stories">
+    Real people, real businesses. These are just a few of the things they’ve said<br
+      class="hidden md:block"
+    />
+    after working with me ›
+    <span
+      class="from-gradient-start to-gradient-end bg-gradient-to-r bg-clip-text font-bold text-transparent"
+      >clear process, real results, and ongoing support.</span
+    >
+  </Header>
+</Container>
