1fdc725250609dd00460ae14ab1634538fedc564
jucegt <julio2201@gmail.com>
2025-04-18 23:33:52 -0600

    feat: add experience component to highlight professional background

---
+++ src/components/experience.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Container from './container.svelte';
+  import Header from './section/header.svelte';
+</script>
+
+<Container as="section" class="my-24 flex flex-col md:my-48">
+  <Header badge="Experience" title="Pro-level experience">
+    I’ve worked on projects for well-known companies.<strong>*</strong> That experience helps me
+    bring high standards, clean design, and real results to every business I work with ›
+    <span
+      class="from-gradient-start to-gradient-end bg-gradient-to-r bg-clip-text font-bold text-transparent"
+      >no matter the size.</span
+    >
+  </Header>
+</Container>
