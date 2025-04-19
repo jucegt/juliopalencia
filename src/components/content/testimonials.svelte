00eede8610cef8e1aae7cb3f2f403de7e770066e
jucegt <julio2201@gmail.com>
2025-04-19 17:47:31 -0600

    feat: restructure testimonials component to enhance layout and functionality

--- src/components/content/testimonials.svelte
+++ src/components/content/testimonials.svelte
@@ -1,13 +1,48 @@
 <script>
+  import Carousel from '../carousel.svelte';
   import Container from '../container.svelte';
   import Header from '../section/header.svelte';
   import ShinyText from '../shiny-text.svelte';
+  import Testimonial from '../testimonial.svelte';
+
+  const testimonials = [
+    {
+      id: 1,
+      name: 'Carolina',
+      business: 'Small business owner',
+      testimonial:
+        '“I had no idea how websites worked, but he explained everything step by step. Super helpful.”'
+    },
+    {
+      id: 2,
+      name: 'Luis',
+      business: 'Local business owner',
+      testimonial:
+        '“I just told him what I needed, and he took care of the rest. Now customers contact me online.”'
+    },
+    {
+      id: 3,
+      name: 'Andrea',
+      business: 'Natural products shop',
+      testimonial:
+        '“I liked that he didn’t overwhelm me with tech stuff. Straight to the point and delivered what he promised.”'
+    }
+  ];
 </script>
 
-<Container as="section" class="my-24 flex flex-col md:my-48">
-  <Header badge="Testimonials" title="Client stories">
-    Real people, real businesses. These are just a few of the things they’ve said
-    <br class="hidden md:block" />
-    after working with me › <ShinyText>clear process, real results, and ongoing support.</ShinyText>
-  </Header>
-</Container>
+<div class="overflow-hidden">
+  <Container as="section" class="flex flex-col">
+    <Header badge="Testimonials" title="Client stories">
+      Real people, real businesses. These are just a few of the things they’ve said
+      <br class="hidden md:block" />
+      after working with me › <ShinyText
+        >clear process, real results, and ongoing support.</ShinyText
+      >
+    </Header>
+    <Carousel>
+      {#each testimonials as { testimonial, id, ...rest } (id)}
+        <Testimonial {...rest}>{testimonial}</Testimonial>
+      {/each}
+    </Carousel>
+  </Container>
+</div>
