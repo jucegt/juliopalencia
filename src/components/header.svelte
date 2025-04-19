9669e3ac32fd0bda61b9df7eab432cf3c70c7b93
jucegt <julio2201@gmail.com>
2025-04-18 21:08:02 -0600

    feat: enhance header component with TypeScript support and scroll effect

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
   import { Mail } from '@lucide/svelte';
 
   import Button from './button.svelte';
@@ -6,17 +6,40 @@
   import Logo from './header/logo.svelte';
   import Nav from './header/nav.svelte';
   import Right from './header/right.svelte';
+  import { cn } from '../utilities/cn';
+
+  const SCROLL_LIMIT = 140;
+  const SCROLL_LIMIT_MOBILE = 70;
+  let isAfterPosition = $state(false);
+
+  $effect(() => {
+    const handleScroll = () => {
+      const LIMIT = window.innerWidth < 768 ? SCROLL_LIMIT_MOBILE : SCROLL_LIMIT;
+      isAfterPosition = window.scrollY > LIMIT;
+    };
+
+    window.addEventListener('scroll', handleScroll, { passive: true });
+    return () => window.removeEventListener('scroll', handleScroll);
+  });
 </script>
 
 <header
-  class="before:content-'' before:bg-light/70 before:dark:bg-dark/70 sticky top-0 z-50 before:absolute before:inset-0 before:opacity-0 before:backdrop-blur-sm"
+  class={cn(
+    "before:content-'' before:bg-light/80 dark:before:bg-dark/80 sticky top-0 z-50 before:absolute before:inset-0 before:opacity-0 before:backdrop-blur-sm before:transition",
+    isAfterPosition && 'before:opacity-100'
+  )}
 >
   <Container class="flex w-full items-center justify-between py-4 md:py-8">
     <Logo />
     <Nav />
     <Right>
       <Button variant="secondary" href="#contact">
-        <Mail width="24" class="z-10 shrink-0 grow-0 basis-[24px]" aria-hidden="true" />
+        <Mail
+          size="24"
+          strokeWidth="2"
+          class="z-10 shrink-0 grow-0 basis-[24px]"
+          aria-hidden="true"
+        />
         <span class="z-10">
           Let’s talk <span class="hidden md:inline">about your website</span>
         </span>
