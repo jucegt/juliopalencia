205854a710e85f9879988ed0af1d0cc6c1615c3a
jucegt <julio2201@gmail.com>
2025-04-18 20:22:08 -0600

    feat: add glow component for hero section with animated background effects

---
+++ src/components/hero/glow.svelte
@@ -0,0 +1,15 @@
+<div
+  class="pointer-events-none absolute inset-0 flex items-center justify-center"
+  aria-hidden="true"
+>
+  <div
+    class="animate-spin-slow relative h-[40dvw] w-[40dvw] sm:h-[50dvw] sm:w-[50dvw] xl:h-[60dvw] xl:w-[60dvw]"
+  >
+    <div
+      class="bg-purple absolute top-[40px] left-[70%] aspect-square w-[100px] rounded-full blur-[80px] sm:w-[180px] sm:blur-[110px] lg:w-xs lg:blur-[200px] dark:blur-[120px] sm:dark:blur-[165px] lg:dark:blur-[300px]"
+    ></div>
+    <div
+      class="bg-skyblue absolute right-[70%] bottom-[40px] aspect-square w-[100px] rounded-full blur-[80px] sm:w-[180px] sm:blur-[110px] lg:w-xs lg:blur-[200px] dark:blur-[120px] sm:dark:blur-[165px] lg:dark:blur-[300px]"
+    ></div>
+  </div>
+</div>
