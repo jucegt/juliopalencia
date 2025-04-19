ca9eff906e6d07f17a4f8b5d4be9ca4c90d45df4
jucegt <julio2201@gmail.com>
2025-04-19 17:48:43 -0600

    fix: adjust text size for name and Quote component in testimonial layout

--- src/components/testimonial.svelte
+++ src/components/testimonial.svelte
@@ -14,13 +14,17 @@
       <div class="flex items-center justify-between gap-4">
         <div class="shrink-1 grow-1 basis-0">
           {#if name}
-            <p class="font-onest text-dark font-bold">{name}</p>
+            <p class="font-onest text-dark text-base font-bold">{name}</p>
           {/if}
           {#if business}
             <p>{business}</p>
           {/if}
         </div>
-        <Quote class="text-primary shrink-0 grow-0 basis-[24px]" size="24" strokeWidth="2" />
+        <Quote
+          class="text-primary shrink-0 grow-0 basis-[24px] text-base"
+          size="24"
+          strokeWidth="2"
+        />
       </div>
     </footer>
   {/if}
