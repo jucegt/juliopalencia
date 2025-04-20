5e606ec5e9e8ee0cbdbc087f61e03a521202dfec
jucegt <julio2201@gmail.com>
2025-04-19 20:26:46 -0600

    feat: add swipe component with animated hand SVG for user interaction

---
+++ src/components/swipe.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Hand from '../svgs/hand.svelte';
+  import { cn } from '../utilities/cn';
+</script>
+
+<p
+  {...$$restProps}
+  class={cn(
+    'mt-4 flex items-center justify-center text-center text-sm font-bold',
+    $$restProps.class
+  )}
+>
+  Swipe to see more
+  <span
+    class="before:content-'' before:animate-swipe-dot before:bg-light-200 relative ml-2 origin-bottom before:invisible before:absolute before:top-0 before:left-[15px] before:h-[6px] before:w-[6px] before:rounded-full"
+  >
+    <Hand class="animate-swipe-hand relative" size={24} strokeWidth={2} />
+  </span>
+</p>
