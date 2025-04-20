1d6a39519bfec3677717a329e7bfffadafc8badb
jucegt <julio2201@gmail.com>
2025-04-19 20:27:19 -0600

    feat: enhance Title component to accept additional class properties via rest props

--- src/components/title.svelte
+++ src/components/title.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
+  import { cn } from '../utilities/cn';
+
   export let as: keyof HTMLElementTagNameMap = 'h2';
 </script>
 
 <svelte:element
   this={as}
-  class="text-dark dark:text-light font-onest mb-8 text-2xl font-black sm:text-3xl md:text-center md:text-5xl"
+  {...$$restProps}
+  class={cn(
+    'text-dark dark:text-light font-onest mb-8 text-2xl font-black sm:text-3xl md:text-center md:text-5xl',
+    $$restProps.class
+  )}
 >
   <slot />
 </svelte:element>
