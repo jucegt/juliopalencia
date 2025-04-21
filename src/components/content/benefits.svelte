4ae1bcbfe4ce7b72235bfd2cd794a65bd5ece29f
jucegt <julio2201@gmail.com>
2025-04-20 19:31:40 -0600

    fix: add aria-labelledby attributes for improved accessibility in benefits section

--- src/components/content/benefits.svelte
+++ src/components/content/benefits.svelte
@@ -4,8 +4,8 @@
   import ShinyText from '../shiny-text.svelte';
 </script>
 
-<Container as="section" class="my-24 flex flex-col md:my-48">
-  <Header badge="Benefits" title="Why have a website">
+<Container as="section" class="my-24 flex flex-col md:my-48" aria-labelledby="benefits-heading">
+  <Header badge="Benefits" title="Why have a website" id="benefits-heading">
     A good website makes you easier to find, builds trust, and helps more people reach you. These
     are just a few of the ways I make that happen › <ShinyText>without tech headaches.</ShinyText>
   </Header>
