49d636e439e97d37142c5a1ea9f4608c5c6763cd
jucegt <julio2201@gmail.com>
2025-04-20 19:15:48 -0600

    feat: create logo component with responsive dark mode support

---
+++ src/components/logo.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { cn } from '../utilities/cn';
+
+  export let id: string = '';
+</script>
+
+<div
+  {...$$restProps}
+  role="img"
+  class={cn(
+    'mx-4 aspect-square w-37 bg-cover opacity-25',
+    `dark:logo-${id}`,
+    `logo-${id}`,
+    $$restProps.class
+  )}
+></div>
