909dc84be28cc4a14d5002afb27a8c5e1b5c4673
jucegt <julio2201@gmail.com>
2025-04-21 00:12:34 -0600

    feat: create small-title component with TypeScript support

---
+++ src/components/section/small-title.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { cn } from '../../utilities/cn';
+
+  export let as: string = 'h3';
+</script>
+
+<svelte:element
+  this={as}
+  {...$$restProps}
+  class={cn('font-onest text-dark flex gap-4 text-2xl/8 font-bold', $$restProps.class)}
+>
+  <slot />
+</svelte:element>
