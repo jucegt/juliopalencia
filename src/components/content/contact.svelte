8f560458edf502856d24fbdf55c3f0a33464b632
jucegt <julio2201@gmail.com>
2025-04-20 21:17:48 -0600

    feat: add testimonial section with trusted-bullet components for enhanced user engagement

--- src/components/content/contact.svelte
+++ src/components/content/contact.svelte
@@ -1,7 +1,10 @@
 <script>
+  import { Clock, MessageCircleMore, ShieldCheck } from '@lucide/svelte';
   import Container from '../container.svelte';
   import Header from '../section/header.svelte';
   import ShinyText from '../shiny-text.svelte';
+  import Testimonial from '../testimonial.svelte';
+  import TrustedBullet from '../trusted-bullet.svelte';
 </script>
 
 <Container as="section" class="my-24 flex flex-col md:my-48" aria-labelledby="contact-heading">
@@ -12,4 +15,33 @@
     You can share your idea or just ask a question ›
     <ShinyText>I’ll answer you directly.</ShinyText>
   </Header>
+  <div class="gap- flex flex-wrap items-center gap-8 lg:gap-16">
+    <div class="shrink-0 grow-0 basis-full lg:basis-[46%] xl:basis-[34%]">
+      <Testimonial name="Javier" business="Small business owner">
+        “He helped me get a clean, well-made website up in just a few days. I always felt like he
+        was just a message away if I had questions.”
+      </Testimonial>
+      <TrustedBullet class="mt-8 lg:mt-16">
+        <Clock class="text-primary shrink-0 grow-0 basis-[24px]" size="24" strokeWidth="2" />
+        <span>I usually reply within 24 hours</span>
+      </TrustedBullet>
+      <TrustedBullet class="mt-4 lg:mt-8">
+        <MessageCircleMore
+          class="text-primary shrink-0 grow-0 basis-[24px]"
+          size="24"
+          strokeWidth="2"
+        />
+        <span>
+          I’ll reach out via WhatsApp or email › <ShinyText>whichever you prefer</ShinyText>
+        </span>
+      </TrustedBullet>
+      <TrustedBullet class="mt-4 lg:mt-8">
+        <ShieldCheck class="text-primary shrink-0 grow-0 basis-[24px]" size="24" strokeWidth="2" />
+        <span>
+          Your info stays private › <ShinyText>I don’t share it with anyone</ShinyText>
+        </span>
+      </TrustedBullet>
+    </div>
+    <div class="shrink-1 grow-1 basis-full lg:basis-0"></div>
+  </div>
 </Container>
