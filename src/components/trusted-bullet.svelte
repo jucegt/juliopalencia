837affbd01566809b656eef1025be23594a854ce
jucegt <julio2201@gmail.com>
2025-04-20 21:17:34 -0600

    feat: add trusted-bullet component for consistent styling and layout

---
+++ src/components/trusted-bullet.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { cn } from '../utilities/cn';
+</script>
+
+<p
+  {...$$restProps}
+  class={cn('text-dark font-onest flex gap-4 text-base font-bold', $$restProps.class)}
+>
+  <slot />
+</p>
