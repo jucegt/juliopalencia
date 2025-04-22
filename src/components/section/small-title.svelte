8b4b77a1a8934c29cb97a3888174292af47f4e2d
jucegt <julio2201@gmail.com>
2025-04-21 23:51:36 -0600

    fix: add dark mode support to small-title component styling

--- src/components/section/small-title.svelte
+++ src/components/section/small-title.svelte
@@ -7,7 +7,10 @@
 <svelte:element
   this={as}
   {...$$restProps}
-  class={cn('font-onest text-dark flex gap-4 text-xl/6 font-bold md:text-2xl/8', $$restProps.class)}
+  class={cn(
+    'font-onest text-dark dark:text-light flex gap-4 text-xl/6 font-bold md:text-2xl/8',
+    $$restProps.class
+  )}
 >
   <slot />
 </svelte:element>
