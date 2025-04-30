8f5259f209ff30eedf8f92cfd28e8455eb0cdcb9
jucegt <julio2201@gmail.com>
2025-04-30 01:24:54 -0600

    feat: implement localization for benefits section

--- src/components/content/benefits.svelte
+++ src/components/content/benefits.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
+  import { t } from '../../i18n/config';
   import Container from '../container.svelte';
   import Header from '../section/header.svelte';
   import ShinyText from '../shiny-text.svelte';
 </script>
 
 <Container as="section" class="my-24 flex flex-col md:my-48" aria-labelledby="benefits-heading">
-  <Header badge="Benefits" title="Why have a website" id="benefits-heading">
-    A good website makes you easier to find, builds trust, and helps more people reach you. These
-    are just a few of the ways I make that happen › <ShinyText>without tech headaches.</ShinyText>
+  <Header badge={$t('benefits.title')} title={$t('benefits.subtitle')} id="benefits-heading">
+    {$t('benefits.description.text')}
+    <ShinyText>{$t('benefits.description.shiny')}</ShinyText>
   </Header>
 </Container>
