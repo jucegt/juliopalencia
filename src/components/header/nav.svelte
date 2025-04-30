2bf59ffcc9cbc83596f1069d58086a1f98391917
jucegt <julio2201@gmail.com>
2025-04-30 00:43:06 -0600

    feat: update navigation links to use localization for dynamic text rendering

--- src/components/header/nav.svelte
+++ src/components/header/nav.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { t } from '../../i18n/config';
   import Link from './link.svelte';
 </script>
 
@@ -6,7 +7,7 @@
   class="font-figtree border-light-200 dark:border-dark-200 z-10 mr-auto ml-8 hidden h-12 items-center gap-x-8 border-l pl-8 text-base font-medium md:flex"
   aria-label="Main Menu"
 >
-  <Link href="#benefits">Benefits</Link>
-  <Link href="#services">Services</Link>
-  <Link href="#contact">Contact</Link>
+  <Link href="#benefits">{$t('nav.benefits')}</Link>
+  <Link href="#services">{$t('nav.services')}</Link>
+  <Link href="#contact">{$t('nav.contact')}</Link>
 </nav>
