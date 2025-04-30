0a80c270f88dc5152117ea48b6606d951cf8b966
jucegt <julio2201@gmail.com>
2025-04-30 01:32:39 -0600

    feat: implement localization for experience section

--- src/components/content/experience.svelte
+++ src/components/content/experience.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { t } from '../../i18n/config';
   import Container from '../container.svelte';
   import Logo from '../logo.svelte';
   import Header from '../section/header.svelte';
@@ -45,10 +46,9 @@
 </script>
 
 <Container as="section" class="my-24 flex flex-col md:my-48" aria-labelledby="experience-heading">
-  <Header badge="Experience" title="Pro-level experience" id="experience-heading">
-    I’ve worked on projects for well-known companies.<strong>*</strong> That experience helps me
-    bring high standards, clean design, and real results to every business I work with ›
-    <ShinyText>no matter the size.</ShinyText>
+  <Header badge={$t('experience.title')} title={$t('experience.subtitle')} id="experience-heading">
+    {$t('experience.description.text')}
+    <ShinyText>{$t('experience.description.shiny')}</ShinyText>
   </Header>
   <div
     class="before:content-'' after:content-'' before:from-light before:to-light/0 after:from-light after:to-light/0 dark:before:from-dark dark:before:to-dark/0 dark:after:from-dark dark:after:to-dark/0 relative overflow-hidden before:absolute before:top-0 before:left-0 before:z-10 before:h-37 before:w-10 before:bg-gradient-to-r after:absolute after:top-0 after:right-0 after:h-37 after:w-10 after:bg-gradient-to-l md:before:aspect-square md:before:w-37 md:after:aspect-square md:after:w-37"
@@ -63,7 +63,6 @@
     </div>
   </div>
   <p class="mt-4 text-center text-sm text-balance opacity-50">
-    *Logos represent companies I’ve worked with through teams, agencies or subcontracting. Their use
-    does not imply a direct commercial relationship or official endorsement.
+    {$t('experience.disclaimer')}
   </p>
 </Container>
