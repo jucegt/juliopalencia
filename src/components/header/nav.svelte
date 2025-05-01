d42bb948eec5a0b376b88440c237f6fb8c1a1df5
jucegt <julio2201@gmail.com>
2025-04-30 23:13:27 -0600

    feat: enable smooth scrolling for navigation links

--- src/components/header/nav.svelte
+++ src/components/header/nav.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { smoothScroll } from '../../actions/smooth-scroll';
   import { t } from '../../i18n/config';
   import Link from './link.svelte';
 </script>
@@ -6,6 +7,7 @@
 <nav
   class="font-figtree border-light-200 dark:border-dark-200 z-10 mr-auto ml-8 hidden h-12 items-center gap-x-8 border-l pl-8 text-base font-medium md:flex"
   aria-label="Main Menu"
+  use:smoothScroll
 >
   <Link href="#benefits">{$t('nav.benefits')}</Link>
   <Link href="#services">{$t('nav.services')}</Link>
