5947841fa3898283854eb7718b54044671aaacc8
jucegt <julio2201@gmail.com>
2025-04-30 19:29:51 -0600

    feat: enhance carousel component with initialization handling and type imports

--- src/components/carousel.svelte
+++ src/components/carousel.svelte
@@ -1,16 +1,22 @@
 <script lang="ts">
-  import { type EmblaOptionsType } from 'embla-carousel';
+  import { type EmblaCarouselType, type EmblaOptionsType } from 'embla-carousel';
   import emblaCarouselSvelte from 'embla-carousel-svelte';
   import AutoHeight from 'embla-carousel-auto-height';
 
   import Swipe from './section/swipe.svelte';
+  import { carousels } from '../stores/services';
 
+  export let id: string;
   export let role: string | undefined = undefined;
   export let breakpoints: Record<string, EmblaOptionsType> | null = {
     '(min-width: 1280px)': {
       active: false
     }
   };
+
+  function onInit(event: CustomEvent<EmblaCarouselType>): void {
+    carousels.setInstance(id, event.detail);
+  }
 
   let params = {
     options: {
@@ -22,7 +28,7 @@
   };
 </script>
 
-<div use:emblaCarouselSvelte={params}>
+<div use:emblaCarouselSvelte={params} onemblaInit={onInit}>
   <div class="flex items-center gap-x-8 transition-[height] ease-in-out" {role}>
     <slot />
   </div>
