c10f48a27c0601d4c8dec7704cf8c4b5dc2f90d9
jucegt <julio2201@gmail.com>
2025-04-30 23:41:04 -0600

    refactor: update badge component to use $props for improved prop handling

--- src/components/section/badge.svelte
+++ src/components/section/badge.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
   import { cn } from '../../utilities/cn';
 
-  export let as: keyof HTMLElementTagNameMap = 'h1';
+  let { as = 'h1', children, ...restProps } = $props();
 </script>
 
 <svelte:element
   this={as}
-  {...$$restProps}
+  {...restProps}
   class={cn(
     'border-primary text-primary bg-primary/8 font-onest w-fit rounded-full border px-2 py-1 text-(length:--font-xxs) leading-none font-bold uppercase',
-    $$restProps.class
+    restProps.class
   )}
 >
-  <slot />
+  {@render children()}
 </svelte:element>
