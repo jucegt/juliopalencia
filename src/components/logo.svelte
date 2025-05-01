f0a4ecbed042f48b1af0a15ca3ba2dd01c9f8006
jucegt <julio2201@gmail.com>
2025-04-30 23:34:59 -0600

    refactor: logo component to use $props for improved prop handling

--- src/components/logo.svelte
+++ src/components/logo.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
   import { cn } from '../utilities/cn';
 
-  export let id: string = '';
+  let { id = '', ...restProps } = $props();
 </script>
 
 <div
-  {...$$restProps}
+  {...restProps}
   role="img"
   class={cn(
     'aspect-square w-37 bg-cover opacity-25 md:mx-4',
     `dark:logo-${id}`,
     `logo-${id}`,
-    $$restProps.class
+    restProps.class
   )}
 ></div>
