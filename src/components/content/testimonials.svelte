435647f6f6179743a61a3a1957aee6265524f01b
jucegt <julio2201@gmail.com>
2025-04-30 19:59:14 -0600

    feat: refactor testimonials component to use external data and improve localization

--- src/components/content/testimonials.svelte
+++ src/components/content/testimonials.svelte
@@ -1,46 +1,27 @@
 <script lang="ts">
+  import { t, locale } from '../../i18n/config';
   import Carousel from '../carousel.svelte';
   import Container from '../container.svelte';
   import Header from '../section/header.svelte';
   import ShinyText from '../shiny-text.svelte';
   import Testimonial from '../section/testimonial.svelte';
+  import testimonials from '../../data/testimonials';
 
-  const testimonials = [
-    {
-      id: 1,
-      name: 'Carolina',
-      business: 'Small business owner',
-      testimonial:
-        '“I had no idea how websites worked, but he explained everything step by step. Super helpful.”'
-    },
-    {
-      id: 2,
-      name: 'Luis',
-      business: 'Local business owner',
-      testimonial:
-        '“I just told him what I needed, and he took care of the rest. Now customers contact me online.”'
-    },
-    {
-      id: 3,
-      name: 'Andrea',
-      business: 'Natural products shop',
-      testimonial:
-        '“I liked that he didn’t overwhelm me with tech stuff. Straight to the point and delivered what he promised.”'
-    }
-  ];
+  const list = testimonials[($locale as 'en' | 'es') || 'es'];
 </script>
 
 <div class="overflow-hidden">
   <Container as="section" class="flex flex-col" aria-labelledby="testimonials-heading">
-    <Header badge="Testimonials" title="Client stories" id="testimonials-heading">
-      Real people, real businesses. These are just a few of the things they’ve said
-      <br class="hidden md:block" />
-      after working with me › <ShinyText
-        >clear process, real results, and ongoing support.</ShinyText
-      >
+    <Header
+      badge={$t('testimonials.title')}
+      title={$t('testimonials.subtitle')}
+      id="testimonials-heading"
+    >
+      {$t('testimonials.description.text')}
+      <ShinyText>{$t('testimonials.description.shiny')}</ShinyText>
     </Header>
     <Carousel role="list" id="testimonials-carousel">
-      {#each testimonials as { testimonial, id, ...rest } (id)}
+      {#each list as { testimonial, id, ...rest } (id)}
         <Testimonial role="listitem" {...rest}>{testimonial}</Testimonial>
       {/each}
     </Carousel>
