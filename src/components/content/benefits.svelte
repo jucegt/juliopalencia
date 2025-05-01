cfe118e271be0fbcc9fb174b2c8222879639687c
jucegt <julio2201@gmail.com>
2025-04-30 22:30:58 -0600

    fix: add missing id attribute to benefits container for improved accessibility

--- src/components/content/benefits.svelte
+++ src/components/content/benefits.svelte
@@ -5,7 +5,12 @@
   import ShinyText from '../shiny-text.svelte';
 </script>
 
-<Container as="section" class="my-24 flex flex-col md:my-48" aria-labelledby="benefits-heading">
+<Container
+  as="section"
+  class="my-24 flex flex-col md:my-48"
+  aria-labelledby="benefits-heading"
+  id="benefits"
+>
   <Header badge={$t('benefits.title')} title={$t('benefits.subtitle')} id="benefits-heading">
     {$t('benefits.description.text')}
     <ShinyText>{$t('benefits.description.shiny')}</ShinyText>
