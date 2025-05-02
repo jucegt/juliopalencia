e0c7c68e071d7e238075e7923ae615d45f8b3fbf
jucegt <julio2201@gmail.com>
2025-05-01 20:20:42 -0600

    refactor: update contact component for localization with i18n support

--- src/components/content/contact.svelte
+++ src/components/content/contact.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
   import { Clock, MessageCircleMore, ShieldCheck } from '@lucide/svelte';
+
+  import { t } from '../../i18n/config';
   import Container from '../container.svelte';
   import Header from '../section/header.svelte';
   import ShinyText from '../shiny-text.svelte';
@@ -13,22 +15,24 @@
   aria-labelledby="contact-heading"
   id="contact"
 >
-  <Header badge="Contact" title="Ready to start?" id="contact-heading">
-    Send me a message and I’ll help you get your business online ›
-    <ShinyText>quickly, clearly, and stress-free.</ShinyText>
+  <Header badge={$t('contact.title')} title={$t('contact.subtitle')} id="contact-heading">
+    {$t('contact.description.text.one')}
+    <ShinyText>{$t('contact.description.shiny.one')}</ShinyText>
     <br class="hidden md:block" />
-    You can share your idea or just ask a question ›
-    <ShinyText>I’ll answer you directly.</ShinyText>
+    {$t('contact.description.text.two')}
+    <ShinyText>{$t('contact.description.shiny.two')}</ShinyText>
   </Header>
   <div class="gap- flex flex-wrap items-center gap-8 lg:gap-16">
     <div class="shrink-0 grow-0 basis-full lg:basis-[46%] xl:basis-[34%]">
-      <Testimonial name="Javier" business="Small business owner">
-        “He helped me get a clean, well-made website up in just a few days. I always felt like he
-        was just a message away if I had questions.”
+      <Testimonial
+        name={$t('contact.testimonial.name')}
+        business={$t('contact.testimonial.business')}
+      >
+        {$t('contact.testimonial.testimonial')}
       </Testimonial>
       <TrustedBullet class="mt-8 lg:mt-16">
         <Clock class="text-primary shrink-0 grow-0 basis-[24px]" size="24" strokeWidth="2" />
-        <span>I usually reply within 24 hours</span>
+        <span>{$t('contact.trustedBullets.one')}</span>
       </TrustedBullet>
       <TrustedBullet class="mt-4 lg:mt-8">
         <MessageCircleMore
@@ -37,13 +41,15 @@
           strokeWidth="2"
         />
         <span>
-          I’ll reach out via WhatsApp or email › <ShinyText>whichever you prefer</ShinyText>
+          {$t('contact.trustedBullets.two.text')}
+          <ShinyText>{$t('contact.trustedBullets.two.shiny')}</ShinyText>
         </span>
       </TrustedBullet>
       <TrustedBullet class="mt-4 lg:mt-8">
         <ShieldCheck class="text-primary shrink-0 grow-0 basis-[24px]" size="24" strokeWidth="2" />
         <span>
-          Your info stays private › <ShinyText>I don’t share it with anyone</ShinyText>
+          {$t('contact.trustedBullets.three.text')}
+          <ShinyText>{$t('contact.trustedBullets.three.shiny')}</ShinyText>
         </span>
       </TrustedBullet>
     </div>
