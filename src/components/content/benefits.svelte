adf76cc0b4c3f1a7a037b4603703b514d1690a93
jucegt <julio2201@gmail.com>
2025-05-03 23:17:06 -0600

    feat: update benefits component to support locale-based data retrieval

--- src/components/content/benefits.svelte
+++ src/components/content/benefits.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
-  import { t } from '$i18n/config';
+  import { t, locale } from '$i18n/config';
   import Container from '$comp/container.svelte';
   import Header from '$comp/section/header.svelte';
   import ShinyText from '$comp/shiny-text.svelte';
   import benefits from '$data/benefits';
   import Benefit from '$comp/section/benefit.svelte';
   import Carousel from '$comp/carousel.svelte';
+
+  const list = benefits[($locale as 'en' | 'es', 'es')];
 </script>
 
 <div class="my-24 overflow-hidden md:my-48">
@@ -26,7 +28,7 @@
       class="flex items-stretch gap-4 md:gap-8 lg:grid lg:auto-rows-auto lg:grid-cols-3"
       swipeClass="lg:hidden"
     >
-      {#each benefits as { id, button, ...restProps } (id)}
+      {#each list as { id, button, ...restProps } (id)}
         <Benefit buttonText={button?.text} buttonIcon={button?.icon} {...restProps} />
       {/each}
     </Carousel>
