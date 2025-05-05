19c047a9488728636e7b418b6a2c760661586107
jucegt <julio2201@gmail.com>
2025-05-04 19:17:55 -0600

    feat: add Input component for enhanced form handling and user interaction

---
+++ src/components/input.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import { type Icon as IconType } from '@lucide/svelte';
+
+  import { cn } from '$util/cn';
+
+  type InputProps = {
+    id: string;
+    name: string;
+    label: string;
+    type?: string;
+    placeholder?: string;
+    class?: string;
+    icon?: typeof IconType;
+    invalid?: boolean;
+  };
+
+  let { id, label, icon, invalid, ...restProps }: InputProps = $props();
+
+  const Icon = icon;
+</script>
+
+<div class={cn(restProps.class)}>
+  <label for={id} class="text-dark font-onest mb-2 block font-bold">{label}</label>
+  <div class="relative">
+    <input
+      {...restProps}
+      {id}
+      class={cn(
+        'peer bg-light-50 border-light-200 text-dark focus:ring-primary h-12 w-full rounded-lg border placeholder:text-gray-200 focus:ring-2 focus:ring-offset-2',
+        icon && 'pl-12',
+        invalid && 'ring-2 ring-red-400 ring-offset-2'
+      )}
+    />
+    {#if icon}
+      <Icon
+        class="peer-focus:text-primary pointer-events-none absolute top-[13px] left-[15px] size-6 text-gray-200"
+      />
+    {/if}
+  </div>
+</div>
