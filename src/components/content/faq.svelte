5ba962b64585f10e4d54c56671bda7f0b1171729
jucegt <julio2201@gmail.com>
2025-05-06 00:11:07 -0600

    feat: add FAQ component with accordion functionality for improved user experience

---
+++ src/components/content/faq.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import { locale } from '$i18n/config';
+  import Accordion from '$comp/accordion.svelte';
+  import Container from '$comp/container.svelte';
+  import faq from '$data/faq';
+  import Header from '$comp/section/header.svelte';
+  import ShinyText from '$comp/shiny-text.svelte';
+
+  const list = faq[($locale as 'en' | 'es') || 'es'];
+  const firstPart = list.slice(0, 3);
+  const secondPart = list.slice(3, 6);
+</script>
+
+<Container as="section" class="flex flex-col" aria-labelledby="faq-heading">
+  <Header badge="FAQ" title="What people usually ask me" id="faq-heading">
+    From pricing to timelines, these are the most common questions clients ask me ›
+    <ShinyText>answered in a clear and straightforward way.</ShinyText>
+  </Header>
+  <div class="grid gap-4 lg:gap-8 xl:grid-cols-2">
+    <div class="flex flex-col gap-4 lg:gap-8">
+      {#each firstPart as { id, ...restProps } (id)}
+        <Accordion {...restProps} />
+      {/each}
+    </div>
+    <div class="flex flex-col gap-4 lg:gap-8">
+      {#each secondPart as { id, ...restProps } (id)}
+        <Accordion {...restProps} />
+      {/each}
+    </div>
+  </div>
+</Container>
