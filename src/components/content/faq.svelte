4b43581787d3d240f9b385a5c2f343fbed6e7141
jucegt <julio2201@gmail.com>
2025-05-06 17:57:04 -0600

    feat: implement localization for FAQ header and description

--- src/components/content/faq.svelte
+++ src/components/content/faq.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { locale } from '$i18n/config';
+  import { t, locale } from '$i18n/config';
   import Accordion from '$comp/accordion.svelte';
   import Container from '$comp/container.svelte';
   import faq from '$data/faq';
@@ -12,9 +12,9 @@
 </script>
 
 <Container as="section" class="flex flex-col" aria-labelledby="faq-heading">
-  <Header badge="FAQ" title="What people usually ask me" id="faq-heading">
-    From pricing to timelines, these are the most common questions clients ask me ›
-    <ShinyText>answered in a clear and straightforward way.</ShinyText>
+  <Header badge={$t('faq.title')} title={$t('faq.subtitle')} id="faq-heading">
+    {$t('faq.description.text')}
+    <ShinyText>{$t('faq.description.shiny')}</ShinyText>
   </Header>
   <div class="grid gap-4 lg:gap-8 xl:grid-cols-2">
     <div class="flex flex-col gap-4 lg:gap-8">
