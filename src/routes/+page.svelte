eaefe1eb7a7381090671d0535fab965689648ff7
jucegt <julio2201@gmail.com>
2025-05-06 00:11:19 -0600

    feat: add FAQ component to main page for enhanced user navigation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
   import Benefits from '$comp/content/benefits.svelte';
   import Contact from '$comp/content/contact.svelte';
   import Experience from '$comp/content/experience.svelte';
+  import Faq from '$comp/content/faq.svelte';
   import Header from '$comp/header.svelte';
   import Hero from '$comp/hero.svelte';
   import Services from '$comp/content/services.svelte';
@@ -14,4 +15,5 @@
 <Services />
 <Testimonials />
 <Experience />
+<Faq />
 <Contact />
