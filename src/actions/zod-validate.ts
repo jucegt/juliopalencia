ca1d22184b8aabd21fda7e93da9691853d6d73d4
jucegt <julio2201@gmail.com>
2025-05-08 00:07:46 -0600

    feat: refactor zodValidate action to use structured parameters for improved clarity

--- src/actions/zod-validate.ts
+++ src/actions/zod-validate.ts
@@ -1,26 +1,41 @@
 import { inputValidations } from '$util/validations';
 import type { Action } from 'svelte/action';
 
-export const zodValidate: Action<HTMLInputElement, keyof typeof inputValidations> = (
-  node,
-  type
-) => {
-  const validate = () => {
-    const value = node.value;
-    const schema = inputValidations[type];
+type InputType = keyof typeof inputValidations;
 
-    const result = schema.safeParse(value);
-    node.setCustomValidity(result.success ? '' : result.error.issues[0].message);
+type Params = {
+  type: string;
+  setInvalid: (value: boolean) => void;
+};
+
+export const zodValidate: Action<HTMLInputElement, Params> = (node, params) => {
+  let { type, setInvalid } = params;
+  let isEnabled = type && type in inputValidations;
+
+  const validate = () => {
+    if (!isEnabled || !type) return;
+
+    const schema = inputValidations[type as InputType];
+    const result = schema.safeParse(node.value);
+
+    setInvalid(node.value ? !result.success : false);
+    node.setCustomValidity(' ');
   };
 
-  node.addEventListener('input', validate);
+  if (isEnabled) {
+    node.addEventListener('input', validate);
+  }
 
   return {
-    update(newType) {
-      type = newType;
+    update(newParams) {
+      type = newParams.type;
+      setInvalid = newParams.setInvalid;
+      isEnabled = type && type in inputValidations;
     },
     destroy() {
-      node.removeEventListener('input', validate);
+      if (isEnabled) {
+        node.removeEventListener('input', validate);
+      }
     }
   };
 };
