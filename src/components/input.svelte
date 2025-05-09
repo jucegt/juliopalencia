c0bd40feee3c5d2c34759fda82e3636c8133511a
jucegt <julio2201@gmail.com>
2025-05-08 21:28:07 -0600

    fix: enhance input component styles for improved dark mode support

--- src/components/input.svelte
+++ src/components/input.svelte
@@ -36,7 +36,7 @@
 </script>
 
 <div class={cn(restProps.class)}>
-  <label for={id} class="text-dark font-onest mb-2 block font-bold">{label}</label>
+  <label for={id} class="text-dark font-onest dark:text-light mb-2 block font-bold">{label}</label>
   <div class="relative">
     <input
       {...restProps}
@@ -44,14 +44,14 @@
       use:zodValidate={{ type: restProps.type, setInvalid }}
       onchange={handleChange}
       class={cn(
-        'peer bg-light-50 border-light-200 text-dark focus:ring-primary h-12 w-full rounded-lg border placeholder:text-gray-200 focus:ring-2 focus:ring-offset-2',
+        'peer bg-light-50 ring-offset-light-200 border-light-200 text-dark dark:text-light focus:ring-primary dark:bg-dark-50 dark:border-dark-200 dark:ring-offset-dark-100 dark:placeholder:text-dark-placeholder h-12 w-full rounded-lg border placeholder:text-gray-200 focus:ring-2 focus:ring-offset-2',
         icon && 'pl-12',
         ((invalid && !hasChanged) || localInvalid) && 'ring-2 ring-red-400 ring-offset-2'
       )}
     />
     {#if icon}
       <Icon
-        class="peer-focus:text-primary pointer-events-none absolute top-[13px] left-[15px] size-6 text-gray-200"
+        class="peer-focus:text-primary dark:text-dark-placeholder pointer-events-none absolute top-[13px] left-[15px] size-6 text-gray-200"
       />
     {/if}
   </div>
