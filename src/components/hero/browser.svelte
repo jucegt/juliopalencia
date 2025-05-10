36a4c335799fd0af5fa65f33d3ea9e1914ce3f04
jucegt <julio2201@gmail.com>
2025-05-10 00:32:57 -0600

    feat: add rotation effect to MessageCircleMore icon on group hover

--- src/components/hero/browser.svelte
+++ src/components/hero/browser.svelte
@@ -35,7 +35,7 @@
       aria-hidden="true"
       size="24"
       strokeWidth="2"
-      class="z-10 shrink-0 grow-0 basis-[24px]"
+      class="z-10 shrink-0 grow-0 basis-[24px] transition-[rotate] ease-in-out group-hover:rotate-10"
     />
   </Button>
   <ChevronDown
