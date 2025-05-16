b5d28ab008f196983fd8cf8330214ed5591995a1
jucegt <julio2201@gmail.com>
2025-05-15 20:21:51 -0600

    fix: update Header component to include ButtonCta and Nav

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,20 @@
 <script lang="ts">
   import Benefits from '$comp/content/benefits.svelte';
+  import ButtonCta from '$comp/header/button-cta.svelte';
   import Contact from '$comp/content/contact.svelte';
   import Experience from '$comp/content/experience.svelte';
   import Faq from '$comp/content/faq.svelte';
+  import Footer from '$comp/footer.svelte';
   import Header from '$comp/header.svelte';
   import Hero from '$comp/hero.svelte';
+  import Nav from '$comp/header/nav.svelte';
   import Services from '$comp/content/services.svelte';
   import Testimonials from '$comp/content/testimonials.svelte';
-  import Footer from '$comp/footer.svelte';
 </script>
 
-<Header />
+<Header right={ButtonCta}>
+  <Nav />
+</Header>
 <Hero />
 <Benefits />
 <Services />
