87f87656fe490ee441a9b388a49e7667a243844a
jucegt <julio2201@gmail.com>
2025-05-15 21:44:05 -0600

    fix: update logo component styles for improved layout and responsiveness

--- src/components/header/logo.svelte
+++ src/components/header/logo.svelte
@@ -4,8 +4,18 @@
 
 <a
   href="/"
-  class="ring-primary z-10 -mx-2 flex items-center rounded-sm p-2 focus:outline-none focus-visible:ring-2"
   aria-label="Julio Palencia"
+  class="group ring-primary relative z-10 -m-1 flex size-13.5 items-center justify-center rounded-xl p-1 transition-all ease-in-out focus:outline-none focus-visible:ring-2"
 >
-  <Logo class="text-primary block h-[32px] w-[48px]" />
+  <span
+    class="relative block h-full w-full transition-transform duration-300 ease-in-out transform-3d group-focus-within:rotate-y-180 group-hover:rotate-y-180"
+  >
+    <span class="absolute inset-0 flex items-center justify-center backface-hidden">
+      <Logo class="text-primary size-13.5" />
+    </span>
+
+    <span class="absolute inset-0 flex rotate-y-180 items-center justify-center backface-hidden">
+      <img src="/me.png" alt="Julio Palencia" class="size-full object-cover" />
+    </span>
+  </span>
 </a>
