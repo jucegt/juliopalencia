ebdad1616509ee9346bcd23fb879f4671c6f4913
jucegt <julio2201@gmail.com>
2025-05-15 20:35:20 -0600

    feat: add switch-lang component for language switching functionality

---
+++ src/components/switch-lang.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { page } from '$app/state';
+
+  import { t } from '$i18n/config';
+  import Button from './button.svelte';
+
+  const { url } = page;
+  const { protocol, host, pathname } = url;
+
+  let buttonHref = '';
+  if (host.startsWith('en.')) {
+    buttonHref = `${protocol}//${host.replace('en.', '')}${pathname}`;
+  } else {
+    buttonHref = `${protocol}//en.${host}${pathname}`;
+  }
+</script>
+
+<Button variant="square" aria-label={$t('header.lang.label')} href={buttonHref}>
+  {$t('header.lang.text')}
+</Button>
