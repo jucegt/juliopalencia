06383109ca51747940be86229ebe49cf0baff7c7
jucegt <julio2201@gmail.com>
2025-05-15 20:54:22 -0600

    feat: add switch-theme component for toggling between light and dark themes

---
+++ src/components/switch-theme.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { Moon, Sun } from '@lucide/svelte';
+  import { page } from '$app/state';
+
+  import { t } from '$i18n/config';
+  import { setTheme } from '$util/theme';
+  import Button from './button.svelte';
+
+  const { data } = page;
+  let theme = $state(data.theme);
+
+  function toggleTheme() {
+    const next = theme === 'dark' ? 'light' : 'dark';
+    theme = next;
+    setTheme(next);
+  }
+</script>
+
+<Button variant="square" aria-label={$t('header.theme')} onclick={toggleTheme}>
+  {#if theme === 'dark'}
+    <Sun class="transition-[rotate] ease-in-out group-hover:-rotate-10" size="24" strokeWidth="2" />
+  {:else}
+    <Moon
+      class="transition-[rotate] ease-in-out group-hover:-rotate-10"
+      size="24"
+      strokeWidth="2"
+    />
+  {/if}
+</Button>
