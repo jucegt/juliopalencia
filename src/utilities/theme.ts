9beaa55fee4b6c9bc5615368d5e662da6988294b
jucegt <julio2201@gmail.com>
2025-05-15 21:00:41 -0600

    fix: improve development environment detection in setTheme function

--- src/utilities/theme.ts
+++ src/utilities/theme.ts
@@ -1,9 +1,10 @@
 import { env } from '$env/dynamic/public';
 
 const { PUBLIC_DOMAIN } = env;
-const isDev = PUBLIC_DOMAIN!.includes('localhost');
 
 export function setTheme(theme: string) {
+  const isDev = PUBLIC_DOMAIN?.startsWith('localhost') ?? true;
+
   const domain = isDev ? '' : `Domain=.${PUBLIC_DOMAIN};`;
   const secure = isDev ? '' : 'Secure;';
   document.cookie = `theme=${theme}; Path=/; ${domain} ${secure} Max-Age=31536000; SameSite=Lax`;
