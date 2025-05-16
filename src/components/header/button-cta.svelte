87624a12b57b1a4524268a2f18db26efda953ef8
jucegt <julio2201@gmail.com>
2025-05-15 21:41:18 -0600

    fix: update icon size class for consistency in button-cta component

--- src/components/header/button-cta.svelte
+++ src/components/header/button-cta.svelte
@@ -9,7 +9,7 @@
   <Mail
     size="24"
     strokeWidth="2"
-    class="z-10 shrink-0 grow-0 basis-[24px] transition-[rotate] ease-in-out group-hover:-rotate-10"
+    class="z-10 shrink-0 grow-0 basis-6 transition-[rotate] ease-in-out group-hover:-rotate-10"
     aria-hidden="true"
   />
   <span class="z-10">
